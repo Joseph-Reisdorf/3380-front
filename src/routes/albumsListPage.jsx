06cef60220905f66a94caf85366deed78ec224f3
Peter Shaba <petershababladez@gmail.com>
2024-03-29 17:00:05 -0500

    Update albumsListPage.jsx

--- src/routes/albumsListPage.jsx
+++ src/routes/albumsListPage.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react'; // Simplified imports
 import axios from "axios";
 
+const AlbumCard = ({ albumImage, albumName, artistName }) => {
+  return (
+    <div className="album-card">
+      <img src={albumImage} alt={albumName} className="album-image" />
+      <div className="album-info">
+        <div className="album-name">{albumName}</div>
+        <div className="artist-name">{artistName}</div>
+      </div>
+    </div>
+  );
+};
+
+
 const Album = () => {
   const [album, setAlbum] = useState([]); // Simplified state initialization
 
   useEffect(() => {
     const fetchAlbum = async () => {
       try {
+        //const res = await axios.get(`${process.env.REACT_APP_BACK_URL}/albums/get_albums`);
         const res = await axios.get(`${process.env.REACT_APP_BACK_URL}/albums/get_albums`);
         setAlbum(res.data);
       } catch (error) {
@@ -16,18 +30,25 @@ const Album = () => {
 
     fetchAlbum();
   }, []); // Dependency array added to ensure this effect runs only once
-
   return (
-    <div>
-      <h1>Album</h1>
-      {album.map((a) => (
-        <div className="album" key={a.album_id}>
-          <h3>Name: {a.album_title}</h3>
-          <p>Release Date: {a.album_release_date}</p>
-        </div>
+    <div className="albums-container">
+      {album.map((album, index) => (
+        <AlbumCard
+          key={index}
+          //albumImage={album.album_cover_art}
+          //albumImage={logo}
+          albumName={album.album_title}
+          //artistName={album.artist}
+        />
       ))}
     </div>
   );
 };
 
 export default Album;
+
+
+
+
+
+
