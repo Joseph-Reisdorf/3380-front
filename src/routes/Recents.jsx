b24b08f6948028bbfb75ec913d62be3b3f2b9d5d
notbali <TayabaliMur@outlook.com>
2024-03-29 15:37:08 -0500

    Added recents front end

--- src/routes/Recents.jsx
+++ src/routes/Recents.jsx
@@ -1,15 +1,29 @@
-import React, {useEffect, useState} from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios"; 
 
-function Recents(){
+const Recents = () => {
+    const [recentTracks, setRecentTracks] = useState([]);
 
-    return(
-    
+    useEffect(() => {
+        axios.get("http://localhost:8080/get_recents") 
+            .then(response => {
+                setRecentTracks(response.data); 
+            })
+            .catch(error => {
+                console.error("Error fetching recent tracks:", error);
+            });
+    }, []);
+
+    return (
         <div>
-            <h1>Recents</h1>
-            <Link to="/">Home</Link>
+            <h2>Recent Tracks</h2>
+            <ul>
+                {recentTracks.map(track => (
+                    <li key={track.id}>{track.name}{track.genre}</li> //gets track, name and genre property.
+                ))}
+            </ul>
         </div>
     );
-}
+};
 
 export default Recents;
