1b2e62cb9a36876db9430c83706871d2b05364d7
Peter Shaba <petershababladez@gmail.com>
2024-03-30 02:43:48 -0500

    updated albumsListPage and its routes

--- src/routes/albumsListPage.jsx
+++ src/routes/albumsListPage.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'; // Simplified imports
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 const AlbumCard = ({ albumImage, albumName, artistName }) => {
   return (
     <div className="album-card">
-      <img src={albumImage} alt={albumName} className="album-image" />
+      {/* <img src={albumImage} alt={albumName} className="album-image" /> */}
       <div className="album-info">
         <div className="album-name">{albumName}</div>
         <div className="artist-name">{artistName}</div>
@@ -13,32 +13,30 @@ const AlbumCard = ({ albumImage, albumName, artistName }) => {
   );
 };
 
-
 const Album = () => {
-  const [album, setAlbum] = useState([]); // Simplified state initialization
+  const [album, setAlbum] = useState([]);
 
   useEffect(() => {
     const fetchAlbum = async () => {
       try {
-        //const res = await axios.get(`${process.env.REACT_APP_BACK_URL}/albums/get_albums`);
-        const res = await axios.get(`${process.env.REACT_APP_BACK_URL}/albums/get_albums`);
-        setAlbum(res.data);
+        const res = await axios.get(`${process.env.REACT_APP_BACK_URL}albums/get_albums`);
+        setAlbum(res.data); // Use res.data instead of res.json()
       } catch (error) {
         console.error(error);
-      } 
+      }
     };
 
     fetchAlbum();
-  }, []); // Dependency array added to ensure this effect runs only once
+  }, []); // Dependency array to ensure it runs only once
+
   return (
     <div className="albums-container">
       {album.map((album, index) => (
         <AlbumCard
           key={index}
-          //albumImage={album.album_cover_art}
-          //albumImage={logo}
+          albumImage={album.album_cover_art} // Uncomment and ensure the property name matches
           albumName={album.album_title}
-          //artistName={album.artist}
+          artistName={album.artist} // Uncomment and ensure the property name matches
         />
       ))}
     </div>
@@ -46,9 +44,3 @@ const Album = () => {
 };
 
 export default Album;
-
-
-
-
-
-
