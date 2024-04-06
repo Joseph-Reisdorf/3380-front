9d1d4a43526c036b7f413ded7c994243a96b15d6
notbali <TayabaliMur@outlook.com>
2024-04-06 01:06:20 -0500

    accountPage (still broken)

--- src/routes/accountPage.jsx
+++ src/routes/accountPage.jsx
@@ -1,20 +1,44 @@
-import { React, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/authContext";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-function Account(){
+const Artist = () => {
+  const { id } = useParams(); 
+  const artistId = parseInt(id);
+  const [artistData, setArtistData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const navigate = useNavigate();
-    const { loggedIn } = useAuth();
+  useEffect(() => {
+    const fetchArtistData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BACK_URL}/artists/find_artist_by_id/${artistId}`);
+        setArtistData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-    // return information from the person's account
-    return(
-        <section>
-            
-            {!loggedIn && navigate("/not-logged-in")}
-            <p>Hello Welcome to the Account page</p>
-        </section>
-    );
-}
+    fetchArtistData();
+  }, [artistId]);
 
-export default Account;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching data</div>;
+
+  return (
+    <div>
+      <h2>Artist Details</h2>
+      <div>
+        <strong>Name:</strong> {artistData && artistData.artist_name}
+      </div>
+      <div>
+        <strong>Biography:</strong> {artistData && artistData.artist_biography}
+      </div>
+      {/* Add any other artist details you want to display */}
+    </div>
+  );
+};
+
+export default Artist;
