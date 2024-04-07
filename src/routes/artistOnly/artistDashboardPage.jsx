4dba3a76c4ce7c6dff759f9446dc789a186d528a
notbali <TayabaliMur@outlook.com>
2024-04-07 04:31:57 -0500

    artist dashboard with age distribution of followers

--- src/routes/artistOnly/artistDashboardPage.jsx
+++ src/routes/artistOnly/artistDashboardPage.jsx
@@ -1,52 +1,99 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useIsArtist } from '../../context/authInfo';
 import { useAuth } from '../../context/authContext';
-import {Link} from 'react-router-dom';
+import axios from 'axios';
+import { Chart } from 'chart.js/auto'; 
+import { Routes, Route, Link } from "react-router-dom";
 
 const ArtistDashboardPage = () => {
+    const isArtist = useIsArtist();
+    const { loggedIn, listenerId } = useAuth();
+    const navigate = useNavigate();
+    const [albumData, setAlbumData] = useState([]);
+    const [ageData, setAgeData] = useState([]);
+    const chartRef = useRef(null);
 
-    /*
     useEffect(() => {
-        const fetchArtist = async () => {
+        const fetchAgeData = async () => {
             try {
-                const res = await axios.get(`${process.env.REACT_APP_BACK_URL}/artist/get_artist`);
-                setArtist(res.data);
+                const response = await axios.get(`${process.env.REACT_APP_BACK_URL}/dashboard/get_age_data/artistId`);
+                setAgeData(response.data);
             } catch (error) {
-                console.error(error);
+                console.error('Error fetching listeners ages:', error);
             }
         };
-        fetchArtist();
-    }
-    , []);*/
 
-    const isArtist = useIsArtist();
-    const { loggedIn } = useAuth();
-    const navigate = useNavigate();
-  
-    // // If not an artist, redirect to login page
+        if (loggedIn && isArtist) {
+            fetchAgeData();
+        } else {
+            navigate('/login');
+        }
+    }, [isArtist, loggedIn, listenerId, navigate]);
+
     useEffect(() => {
-      if (!loggedIn) {
-        navigate('/login');
-      }
-      else if (!isArtist) {
-        navigate('/');
-      }
-    }, [isArtist, navigate]); // Depend on isArtist to reactively navigate
-  
+      
+        if (ageData.length > 0) {
+            drawBarGraph();
+        } // eslint-disable-next-line
+    }, [ageData]);
+
+    const drawBarGraph = () => {
+        ageData.sort((a, b) => a.age - b.age) 
+        const albumTitles = ageData.map(age => age.listener_age);
+        const albumLikeCounts = ageData.map(age => age.count);
+    
+        const canvas = document.getElementById('albumLikesChart');
+        const ctx = canvas.getContext('2d');
+    
+    
+        if (chartRef.current !== null) {
+         
+            chartRef.current.destroy();
+        }
+    
+        chartRef.current = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: albumTitles,
+                datasets: [{
+                    label: 'Followers Age Count',
+                    data: albumLikeCounts,
+                    backgroundColor: 'rgba(225, 97, 163, 0.8)',
+                    borderColor: 'rgba(0, 0, 0, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 10, // album like num cap
+                        ticks: {
+                            stepSize: 1,
+                            max: 50
+                        }
+                    }
+                }
+            }
+        });
+    };
+    
+    
+
     return (
-        // if not logged in, redirect to login page
         <div>
-                <div>
-                    <h1>Artist Dashboard</h1>
-                    <Link to="artist_reportspage">
-                <button>View Reports</button>
-            </Link>
-                </div>
+            <h1>Artist Dashboard</h1>
+            <div>
+                <canvas id="albumLikesChart" width="400" height="400"></canvas>
+            </div>
 
+            <div className='add-album'>
         </div>
-
+        </div>
     );
-}
+};
 
 export default ArtistDashboardPage;
