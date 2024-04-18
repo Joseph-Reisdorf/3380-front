54bd30f95ea18d325dbc4a039366532781e40f7c
heyilly <artemis.lapucelle@gmail.com>
2024-04-18 01:32:56 -0500

    these changes not intended for main

--- src/routes/Nav.jsx
+++ src/routes/Nav.jsx
@@ -4,25 +4,14 @@ import '../styles/Nav.css'; // Nav styling file
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; //for the user account icon and notifications icon
 import { faUser, faBell } from '@fortawesome/free-solid-svg-icons'; //for the user account icon and notifications icon
 //import logo from './houston_cougars_logo_secondary_20032603.png';
+import { useAuth } from '../context/authContext.mjs'; //for the user account icon and notifications icon
 
 function Nav(){
+
+    const { loading, userRole } = useAuth();
+
     return(
         <nav>
-<<<<<<< Updated upstream
-            <ul class="navbar">
-                {/* <li>
-                    <Link to="/">
-                        <img src={logo} alt="Logo" /> 
-                    </Link>
-                </li> */}
-                <li><Link to="/" className="home">Home</Link></li>
-                <li><Link to="/recents" className="recents">Recents</Link></li>
-                <li><Link to="/library" className="library">Library</Link></li>
-                <li><Link to="/playlists" className="playlist">Playlists</Link></li>
-                <li><Link to="/albums" className="albums">Albums</Link></li>
-                <li><Link to="/artists" className="artists">Artists</Link></li>
-                <li><Link to="/artist_dashboard" className="artist_dashboard">Dashboard</Link></li>
-=======
             <ul className="navbar">
                 <div className="nav-item">
                     <li><Link to="/" className="home">Home</Link></li>
@@ -45,22 +34,29 @@ function Nav(){
                         </>
                     )}
 
->>>>>>> Stashed changes
 
- 
-                <div class="search-container">
-                    <form action="/search">
-                    <input type="text" placeholder="Search.." name="search"></input>
-                    <button type="submit">Search</button>
-                    </form>
                 </div>
-                <div class="search-container">
-                    <Link to="/account" className="icon-link">
-                        <FontAwesomeIcon icon={faUser} />
-                    </Link>
-                    <Link to="/notifications" className="icon-link">
-                        <FontAwesomeIcon icon={faBell} />
-                    </Link>
+                
+                <div className="nav-item">
+                    <div className="search-container">
+                        {(userRole === 'l' || userRole === 'a') && (
+                            <>
+                                <form action="/search">
+                                <input type="text" placeholder="Search.." name="search"></input>
+                                <button type="submit">Search</button>
+                                </form>
+        
+                                {/*<Link to="/account" className="icon-link">
+                                    <FontAwesomeIcon icon={faUser} />
+                                </Link>*/}
+                                <Link to="/notifications" className="icon-link">
+                                    <FontAwesomeIcon icon={faBell} />
+                            </Link>
+                            </>
+                            
+                        )}
+                        
+                    </div>
                 </div>
             </ul>
         </nav>
